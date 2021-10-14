328b36508f41b42e535b35fbc8e239be0e87a738
wanjema <jonwanjema@gmail.com>
2021-10-14 19:53:03 +0300

    add quotes class

---
+++ src/app/quote.ts
@@ -0,0 +1,4 @@
+export class Quote {
+    constructor(public id: number, public name: string, public author: string,
+        public upvote: number,  public downvote: number ,public person:String, public date:Date,) { }
+}
