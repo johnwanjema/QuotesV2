a29e3a625ce5ee9c27b6d12b319792a48f8356c5
wanjema <jonwanjema@gmail.com>
2021-10-14 21:05:38 +0300

    add timepast pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteDetailsComponent } from './components/quote-details/quote-details.component';
+import { TimePastPipe } from './pipes/time-past.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteDetailsComponent,
+    TimePastPipe,
   ],
   imports: [
     BrowserModule,
