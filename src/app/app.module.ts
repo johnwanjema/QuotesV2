822561909346a7b8edc18c5e8537e92cd1b46290
wanjema <jonwanjema@gmail.com>
2021-10-14 21:17:12 +0300

    add form to add quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteDetailsComponent } from './components/quote-details/quote-details.component';
 import { TimePastPipe } from './pipes/time-past.pipe';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteDetailsComponent,
     TimePastPipe,
+    QuoteFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
